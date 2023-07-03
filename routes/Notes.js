fb295147c31bffcf873669faa0c70a1e10fba4dd
Varun-Kolanu <112728411+Varun-Kolanu@users.noreply.github.com>
2023-07-03 23:06:24 +0530

    Delete Notes

--- routes/Notes.js
+++ routes/Notes.js
@@ -36,10 +36,53 @@ router.get("/getnotes", fetchUser, async (req,res) => {
         const notes = await Note.find({userId})
         res.status(200).json(notes)
     } catch (error) {
-         //* Internal Server error
+        //* Internal Server error
         console.log(error)
         res.status(500).json("Some error occured")
     }
 })
+
+//* ROUTE 3: Update Notes by PUT /api/v1/notes/update. Login required
+router.put('/update/:id', fetchUser,async (req, res) => {
+    try {
+        const noteId = req.params.id
+        let note = await Note.findById(noteId)
+        if(!note) return res.status(404).json({error: "No such Note exists"})
+        const {userId} = req
+        if(userId !== note.userId.toString()) return res.status(400).json({error: "Invalid User"})
+        let newNote = {}
+        const {title,description, tag} = req.body
+        if(title) newNote.title = title
+        if(description) newNote.description = description
+        if(tag) newNote.tag = tag
+        note = await Note.findByIdAndUpdate(noteId, newNote)
+        res.status(200).json(note)
+    } 
+    catch (error) {
+        //* Internal Server error
+        console.log(error)
+        res.status(500).json("Some error occured")
+    }
+})
+
+//* ROUTE 4: Delete Notes by  /api/v1/notes/delete. Login required
+router.delete('/delete/:id', fetchUser, async (req, res) => {
+    try {
+        const noteId = req.params.id
+        let note = await Note.findById(noteId)
+        if(!note) return res.status(404).json({error: "No such Note exists"})
+        const {userId} = req
+        if(userId !== note.userId.toString()) return res.status(400).json({error: "Deletion not allowed"})
+        await Note.findByIdAndDelete(noteId)
+        res.status(200).json({message: "Note Deleted"})
+    } 
+    catch (error) {
+        //* Internal Server error
+        console.log(error)
+        res.status(500).json("Some error occured")
+    }
+})
+
+
 
 export default router
